9ce6fa5aaec79d9ef2891b2eccd58447155d96f3
atestthisis5 <atestthisis5@gmail.com>
2013-10-14 09:20:11 -0400

    complete first set of unit tests

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,16 +1,29 @@
 """
 This file demonstrates writing tests using the unittest module. These will pass
 when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.core.urlresolvers import resolve
+from django.http import HttpRequest
 
+from lists.views import home
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class HomePageTest(TestCase):
+
+    def test_root_url_resolves_to_home_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home)
+        
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+
+        response = home(request)
+        
+        # response_content = response.content
+        # if type(response_content) != str:
+        #     response_content = str(response.content)[2:-1]
+        
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>To-Do Lists</title>', response.content)
+        self.assertTrue(response.content.endswith(b'</html>'))
